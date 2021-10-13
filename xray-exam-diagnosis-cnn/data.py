28f150011c5c8b344f4687c3204a54f14a573078
Santiago Giovagnoli <santigiova@gmail.com>
2021-10-13 15:07:45 -0300

    mend

--- xray-exam-diagnosis-cnn/data.py
+++ xray-exam-diagnosis-cnn/data.py
@@ -57,55 +57,58 @@ def get_data(
 
 
 
-# def get_binary_data(
-#     img_path: str,
-#     labels_file: str,
-#     labels: list = None,
-#     index_col='Image Index',
-#     labels_col='Finding Labels',
-#     return_labels_dict=False,
-#     ):
-#     """
-#     Loads X_data, images, from a directory with files, and assings labels
-#     according to the DF in labels_path.
-#     While loading, filters labels not in 'labels' list
-#     - img_path: relative path to directory containing folders
-#     - labels_file: relative path to file (.csv) containing labels. Must include filename
-#     - labels: list of labels to consider, filtering out every other label.
-#     """
+def get_binary_data(
+    img_path: str,
+    labels_file: str,
+    labels: list = None,
+    index_col='Image Index',
+    labels_col='Finding Labels',
+    return_labels_dict=False,
+    ):
+    """
+    Loads X_data, images, from a directory with files, and assings labels
+    according to the DF in labels_path.
+    While loading, filters labels not in 'labels' list
+    - img_path: relative path to directory containing folders
+    - labels_file: relative path to file (.csv) containing labels. Must include filename
+    - labels: list of labels to consider, filtering out every other label.
+    """
 
-#     # Load y_labels
-#     y = pd.read_csv(labels_file)
-#     y['labels_binary'] = np.where(y['Finding Labels'] != 'No Finding',
-#                                   'Disease', y['Finding Labels'])
-#     y = y[['Image Index', 'labels_binary']]
+    # Load y_labels
+    y = pd.read_csv(labels_file)
+    y['labels_binary'] = np.where(y['Finding Labels'] != 'No Finding',
+                                  'Disease', y['Finding Labels'])
+    y = y[['Image Index', 'labels_binary']]
 
-#     datagen = ImageDataGenerator(rescale=1 / 255, )
+    datagen = ImageDataGenerator(rescale=1 / 255, )
 
-#     train_generator = datagen.flow_from_dataframe(
-#         dataframe=y,
-#         directory=img_path,
-#         x_col=index_col,
-#         y_col=labels_col,
-#         batch_size=32,
-#         shuffle=True,
-#         class_mode="binary",
-#         target_size=(228,
-#                      228),  # according to multiclass classification algorithm
-#         classes= labels  # Filters files that don't belong to these classes
-#     )
+    train_generator = datagen.flow_from_dataframe(
+        dataframe=y,
+        directory=img_path,
+        x_col=index_col,
+        y_col=labels_col,
+        batch_size=32,
+        shuffle=True,
+        class_mode="binary",
+        target_size=(228,
+                     228),  # according to multiclass classification algorithm
+        classes= labels  # Filters files that don't belong to these classes
+    )
 
-#     if return_labels_dict:
-#         return y, train_generator, train_generator.class_indices
-#     else:
-#         return train_generator
+    if return_labels_dict:
+        return y, train_generator, train_generator.class_indices
+    else:
+        return train_generator
 
 
-def filter_datraset_from_list(dataset: pd.DataFrame, filter_list, filter_by_col: str):
+def filter_datraset_from_list(dataset: pd.DataFrame, filter_list: list, filter_by_col: str):
     """
     Return a test/train dataset split from the given index list.
-    -test_set_filter: .csv file with list
+    - test_set_filter: .csv file with list
+    - filter_list = file/df/list containing desired elements for train set
+    - filter by column: column to filter by
     """
+
     if isinstance(filter_list, str):
         values_to_filter = pd.read_csv(filter_list)
     else:
@@ -120,6 +123,23 @@ def filter_datraset_from_list(dataset: pd.DataFrame, filter_list, filter_by_col:
 
     return y_train, y_test
 
+def split_train_val(dataset: pd.DataFrame, column_to_filter, split: float = 0.85):
+    """
+    Randomly split a df, by a given column in a `split` split.
+
+    ex: column_to_filter = 'Patient ID'
+    """
+
+    patients = pd.DataFrame(dataset[column_to_filter].unique(), columns=[column_to_filter])
+
+    train_guide = patients.sample(frac=0.8, random_state=42)
+
+    bool_filter = dataset[column_to_filter].isin(train_guide[column_to_filter])
+
+    y_train = dataset[bool_filter]
+    y_val = dataset[bool_filter.map(lambda x: not x)]
+
+    return y_train, y_val
 
 if __name__ == '__main__':
     import os
@@ -132,3 +152,10 @@ if __name__ == '__main__':
     # labels = 'Data_Entry_2017.csv'  # FUll dataset labels
     pass
     # y, X = get_data(img_path, labels_path,  )
+
+# 0. Get data.
+# 1. Split train - test
+# 2. Split val - train
+# 3. Generate generators
+# 3.1. Binary, apply to image index, and binary labels
+# 3.2. Multicategory, apply to image index, and Processed labels.
